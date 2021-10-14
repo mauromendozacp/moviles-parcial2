f3f18838c10a27e2200e5e0fc6aee6665f913fd5
Mauro <mauro.mendozacp@gmail.com>
2021-10-14 02:05:29 -0300

    Mainmenu implemented

---
+++ Assets/Scripts/UI/MainmenuUI.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainmenuUI : MonoBehaviour
+{
+    #region EXPOSED_FIELDS
+
+    [SerializeField] private GameObject menuPanel = null;
+    [SerializeField] private GameObject creditsPanel = null;
+
+    #endregion
+
+    #region PUBLIC_METHODS
+
+    public void PlayGame()
+    {
+        GameManager.Get().ChangeScene(GameManager.SceneGame.GamePlay);
+    }
+
+    public void ShowMenu()
+    {
+        menuPanel.SetActive(true);
+        creditsPanel.SetActive(false);
+    }
+
+    public void ShowCredits()
+    {
+        menuPanel.SetActive(false);
+        creditsPanel.SetActive(true);
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+
+    #endregion
+}
