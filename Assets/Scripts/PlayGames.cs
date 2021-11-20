3eeb7df4b0149caef705bc7829b042bda7b5040e
Mauro <mauro.mendozacp@gmail.com>
2021-11-20 18:52:50 -0300

    googleplaygames added

---
+++ Assets/Scripts/PlayGames.cs
@@ -0,0 +1,68 @@
+﻿using GooglePlayGames;
+using GooglePlayGames.BasicApi;
+using UnityEngine;
+
+public class PlayGames : MonoBehaviour
+{
+    public int playerScore;
+    string leaderboardID = "";
+
+    public static PlayGamesPlatform platform;
+
+    void Start()
+    {
+        if (platform == null)
+        {
+            PlayGamesClientConfiguration config = new PlayGamesClientConfiguration.Builder().Build();
+            PlayGamesPlatform.InitializeInstance(config);
+            PlayGamesPlatform.DebugLogEnabled = true;
+            platform = PlayGamesPlatform.Activate();
+        }
+
+        Social.Active.localUser.Authenticate(success =>
+        {
+            if (success)
+            {
+                Debug.Log("Logged in successfully");
+            }
+            else
+            {
+                Debug.Log("Login Failed");
+            }
+        });
+
+        UnlockAchievement(0);
+    }
+
+    public void AddScoreToLeaderboard()
+    {
+        if (Social.Active.localUser.authenticated)
+        {
+            Social.ReportScore(playerScore, leaderboardID, success => { });
+        }
+    }
+
+    public void ShowLeaderboard()
+    {
+        if (Social.Active.localUser.authenticated)
+        {
+            platform.ShowLeaderboardUI();
+        }
+    }
+
+    public void ShowAchievements()
+    {
+        if (Social.Active.localUser.authenticated)
+        {
+            platform.ShowAchievementsUI();
+        }
+    }
+
+    public static void UnlockAchievement(int ID)
+    {
+        if (Social.Active.localUser.authenticated)
+        {
+            Social.ReportProgress(GPGSIds.achievement_jaja_noob, 100f, success => { });
+        }
+    }
+}
